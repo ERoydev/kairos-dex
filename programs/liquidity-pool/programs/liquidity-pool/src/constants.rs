8edeb6d6cab400c1753939290c6c5771fb7267ed
ERoydev <e.roydev@gmail.com>
2026-07-23 01:14:03 +0300

    add: features

--- programs/liquidity-pool/programs/liquidity-pool/src/constants.rs
+++ programs/liquidity-pool/programs/liquidity-pool/src/constants.rs
@@ -8,3 +8,17 @@ pub const POOL_VERSION: u8 = 1;
 
 #[constant]
 pub const USDC_VAULT_SEED: &[u8] = b"usdc_vault";
+
+#[constant]
+pub const LP_MINT_SEED: &[u8] = b"lp_mint";
+
+#[constant]
+pub const DEFAULT_DECIMALS: u8 = 6;
+
+#[constant]
+#[cfg(feature = "dev")]
+pub const USDC_MINT: Pubkey = pubkey!("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU");
+
+#[constant]
+#[cfg(not(feature = "dev"))]
+pub const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
